155972bd4dd081b9346abc4a11f203b34e4eda7e
explorerh <helloworld1661@gmail.com>
2018-05-17 01:22:44 -0400

    Using web_router in app.js

--- cnodeSrc/app.js
+++ cnodeSrc/app.js
@@ -5,8 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var engine = require('ejs-mate');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+var webRouter = require('./routes/web_router');
 
 var app = express();
 
@@ -21,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/', webRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- cnodeSrc/routes/web_router.js
+++ cnodeSrc/routes/web_router.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 //display sign up page
 router.get('/signup', function(req, res) {
     res.render('sign/signup');
@@ -14,7 +9,6 @@ router.get('/signup', function(req, res) {
 //submit sign up information
 router.post('/signup', function(req, res) {
     //implement to submit sign up function
-
 });
 
 //display sign in page
@@ -25,13 +19,11 @@ router.get('/signin', function(req, res) {
 //submit sign in information
 router.post('/signin', function(req, res) {
     //implement to submit sign in function
-
 });
 
 //sign out
 router.post('/signout', function(req, res) {
     //implement to submit sign out function 
-
 });
 
 module.exports = router;
