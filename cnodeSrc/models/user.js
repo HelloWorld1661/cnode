3cd0c22bf39846a8df32335782e1331b7636c11c
explorerh <helloworld1661@gmail.com>
2018-05-30 11:24:04 -0400

    minor bug fixed

--- cnodeSrc/models/user.js
+++ cnodeSrc/models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb:127.0.0.1/node_club');
 
 var UserSchema = new mongoose.Schema({
-    username: Staring,
+    username: String,
     pass: String,
     email: String
 });
